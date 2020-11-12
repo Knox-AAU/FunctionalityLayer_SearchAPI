46c48b1788f38770f5b63339f1ee02b2d364eb4e
DeagleBiegel <47975952+DeagleBiegel@users.noreply.github.com>
2020-11-12 10:29:52 +0100

    Updated Document.java

--- src/main/java/searchengine/Document.java
+++ src/main/java/searchengine/Document.java
@@ -4,20 +4,25 @@ import java.util.HashMap;
 
 public class Document {
 
-    private HashMap<String, Integer> vector;
+    private HashMap<String, Integer> termFrequency;
 
-    public Document(String content){
+    public Document(String content) {
 
-        vector = new HashMap<String, Integer>();
+        termFrequency = new HashMap<String, Integer>();
 
         String[] sArray = content.split("\\s+");
 
         for (String s : sArray) {
-            if (vector.containsKey(s)) {
-                vector.put(s, vector.get(s) + 1);
-            } else {
-                vector.put(s, 1);
+            if (termFrequency.containsKey(s)) {
+                termFrequency.put(s, termFrequency.get(s) + 1);
+            }
+            else {
+                termFrequency.put(s, 1);
             }
         }
+    }
+
+    public HashMap<String, Integer> getTermFrequency() {
+        return termFrequency;
     }
 }
