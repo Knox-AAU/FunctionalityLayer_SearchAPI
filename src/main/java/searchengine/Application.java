60a93c1c19b434cab987b44ac9cc156b6ee30bf0
Thomas <tstubk18@student.aau.dk>
2020-11-17 10:39:43 +0100

    added (wrong(?)) calculations

--- src/main/java/searchengine/Application.java
+++ src/main/java/searchengine/Application.java
@@ -3,6 +3,10 @@ package searchengine;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.List;
+
 @SpringBootApplication
 public class Application {
 
@@ -10,6 +14,52 @@ public class Application {
 		//SpringApplication.run(Application.class, args);
 
 		//Document d = new Document("hej hej hej med med dig");
+
+		List<Document> docs = new ArrayList<Document>();
+
+		Document query = new Document("query","is denmark smaller that russia");
+
+		VectorSpaceModel vsm = new VectorSpaceModel();
+
+		docs = vsm.createTestData();
+
+		vsm.createTfidf(docs, query);
+
+		for(Document d : docs){
+			vsm.giveDocumentScore(d, query);
+		}
+
+		System.out.println("TERM FREQ");
+		for(Document d : docs){
+			System.out.println(d.getTitle());
+			for(String term : d.getTermFrequency().keySet()){
+				System.out.println(term + " | " + d.getTermFrequency().get(term));
+			}
+		}
+		System.out.println("q");
+		for(String term : query.getTermFrequency().keySet()){
+			System.out.println(term + " | " + query.getTermFrequency().get(term));
+		}
+		System.out.println();
+
+		System.out.println("TFIDF");
+		for(Document d : docs){
+			System.out.println(d.getTitle());
+			for(String term : d.getTfidf().keySet()){
+				System.out.println(term + " | " + d.getTfidf().get(term));
+			}
+		}
+		System.out.println("q");
+		for(String term : query.getTfidf().keySet()){
+			System.out.println(term + " | " + query.getTfidf().get(term));
+		}
+		System.out.println();
+
+		System.out.println("SCORE");
+		for(Document d : docs){
+			System.out.println(d.getTitle() + " | " + d.getScore());
+		}
+
 
 	}
 
--- src/main/java/searchengine/Document.java
+++ src/main/java/searchengine/Document.java
@@ -6,12 +6,12 @@ public class Document {
 
     private HashMap<String, Integer> termFrequency;
     private HashMap<String, Double> tfidf;
-    private String name;
+    private String title;
     private double score;
 
     public Document(String name, String content) {
 
-        this.name = name;
+        this.title = name;
         termFrequency = new HashMap<String, Integer>();
 
         String[] sArray = content.split("\\s+");
@@ -27,13 +27,20 @@ public class Document {
     }
 
     public void createTfidf(HashMap<String, Double> idf){
+
+        tfidf = new HashMap<String, Double>();
+
         for(String term : termFrequency.keySet()){
-            tfidf.put(term, termFrequency.get(term) * idf.get(term));
+            tfidf.put(term, termFrequency.get(term) * idf.getOrDefault(term, 0.0));
         }
     }
 
     public double getScore(){
         return this.score;
+    }
+
+    public String getTitle(){
+        return title;
     }
 
     public void setScore(double value){
