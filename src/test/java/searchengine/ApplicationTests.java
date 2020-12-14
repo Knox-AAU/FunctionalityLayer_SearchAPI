054f1fc4672c0f0d8d16f833e7e136d96a04300b
Esben Rask Bach <erb18@student.aau.dk>
2020-12-14 10:27:19 +0100

    re-added ApplicationTests from commit 6dc212323e

--- src/test/java/searchengine/ApplicationTests.java
+++ src/test/java/searchengine/ApplicationTests.java
@@ -1,13 +1,20 @@
 package searchengine;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import searchengine.restcontrollers.SearchController;
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 class ApplicationTests {
 
+	@Autowired
+	private SearchController controller;
+
 	@Test
 	void contextLoads() {
+		assertThat(controller).isNotNull();
 	}
 
 }
