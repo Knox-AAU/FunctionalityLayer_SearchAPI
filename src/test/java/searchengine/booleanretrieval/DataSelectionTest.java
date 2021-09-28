da8ec11fe2507bfe2667b599ffc26147ab9c1b85
Jacob <35376760+BjoerklundJacob@users.noreply.github.com>
2021-09-28 10:55:47 +0200

    The test 101

--- src/test/java/searchengine/booleanretrieval/DataSelectionTest.java
+++ src/test/java/searchengine/booleanretrieval/DataSelectionTest.java
@@ -9,6 +9,7 @@ import java.sql.Statement;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
+// See https://javadoc.io/doc/org.mockito/mockito-core/latest/org/mockito/Mockito.html for mockito documentation
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
@@ -17,17 +18,24 @@ class DataSelectionTest {
 
     /** Handles the Arrange part of the tests (can't use @BeforeAll since it will give race condition errors) */
     static DataSelection setup() {
+        // Create mock for the connection the DataSelection needs
         ISqlConnection connection = mock(ISqlConnection.class);
+        // And mocks for the classes returned by the mocked connection
         Statement statement = mock(Statement.class);
         ResultSet resultSet = mock(ResultSet.class);
         try {
+            // Return the mocked statement when calling createStatement on the connection
             when(connection.createStatement()).thenReturn(statement);
+            // Regardless of the query the executeQuery is called with, return the mocked result set
             when(statement.executeQuery(anyString())).thenReturn(resultSet);
+            // The mocked result set has 3 entries (the 4th call will return false indicating there is no next entry in resultSet)
             when(resultSet.next()).thenReturn(true, true, true, false);
+            // The mocked entries have 4 used labels: {"wordname", "amount", "articletitle", "filepath"}
+            // The documents are created based on distinct article titles (so in this mock 2 articles are returned)
             when(resultSet.getString("wordname")).thenReturn("lorem", "ipsum", "dolor");
             when(resultSet.getInt("amount")).thenReturn(5, 4, 3);
-            when(resultSet.getString("articletitle")).thenReturn("testarticle1", "testarticle1","testarticle2");
-            when(resultSet.getString("filepath")).thenReturn("/test/testarticle1", "testarticle1","/test/testarticle2");
+            when(resultSet.getString("articletitle")).thenReturn("testarticle1", "testarticle1", "testarticle2");
+            when(resultSet.getString("filepath")).thenReturn("/test/testarticle1", "testarticle1", "/test/testarticle2");
 
             return new DataSelection(connection);
         }
