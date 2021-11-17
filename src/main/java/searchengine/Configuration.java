2a1d681fe6097f8470611b1ca19ebf831010bfeb
Rasmus Hebsgaard <r.hebsgaard@gmail.com>
2021-11-17 12:35:46 +0100

    Add clarification to Configuration class

--- src/main/java/searchengine/Configuration.java
+++ src/main/java/searchengine/Configuration.java
@@ -7,6 +7,11 @@ import java.io.File;
 import java.io.FileNotFoundException;
 
 public class Configuration {
+    /**
+     * finds env file in current directory and loads from the key given
+     * @param key
+     * @return the value of the key in the .env file
+     */
     public String get(String key) {
         // finds env file in current directory and loads from the key given
         return Dotenv.configure().directory(System.getProperty("user.dir")).load().get(key);
@@ -14,7 +19,7 @@ public class Configuration {
 
     public Configuration() throws FileNotFoundException {
         String expectedPath = System.getProperty("user.dir") + "/.env";
-        System.out.println(System.getProperty("user.dir") + "/.env");
+        System.out.println(".env should be located at: " + System.getProperty("user.dir") + "/.env");
         File envFile = new File(expectedPath);
         if(!envFile.exists()){
             throw new FileNotFoundException(".env file not found at: " + expectedPath);
